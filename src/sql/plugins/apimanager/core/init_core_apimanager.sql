232223ab3441fe5de7b6ba35a3f673be4e3e8e4b
Loris Boiteux <lboiteux@yupiik.com>
2025-09-24 15:28:29 +0200

    [sql] create APIM menu for APIM features

--- src/sql/plugins/apimanager/core/init_core_apimanager.sql
+++ src/sql/plugins/apimanager/core/init_core_apimanager.sql
@@ -1,10 +1,12 @@
+INSERT INTO core_feature_group(id_feature_group,feature_group_description,feature_group_label,feature_group_order,feature_group_icon) VALUES
+('APIM','apimanager.features.group.apim.description','apimanager.features.group.apim.label',8,'ti ti-files');
 
 --
 -- Data for table core_admin_right
 --
 DELETE FROM core_admin_right WHERE id_right = 'APIMANAGER_CLIENT_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('APIMANAGER_CLIENT_MANAGEMENT','apimanager.adminFeature.ManageClients.name',1,'jsp/admin/plugins/apimanager/ManageClients.jsp','apimanager.adminFeature.ManageClients.description',0,'apimanager',NULL,NULL,NULL,4);
+('APIMANAGER_CLIENT_MANAGEMENT','apimanager.adminFeature.ManageClients.name',1,'jsp/admin/plugins/apimanager/ManageClients.jsp','apimanager.adminFeature.ManageClients.description',0,'apimanager','APIM',NULL,NULL,4);
 
 
 --
@@ -19,7 +21,7 @@ INSERT INTO core_user_right (id_right,id_user) VALUES ('APIMANAGER_CLIENT_MANAGE
 --
 DELETE FROM core_admin_right WHERE id_right = 'APIMANAGER_API_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('APIMANAGER_API_MANAGEMENT','apimanager.adminFeature.ManageApis.name',1,'jsp/admin/plugins/apimanager/ManageApis.jsp','apimanager.adminFeature.ManageApis.description',0,'apimanager',NULL,NULL,NULL,4);
+    ('APIMANAGER_API_MANAGEMENT','apimanager.adminFeature.ManageApis.name',1,'jsp/admin/plugins/apimanager/ManageApis.jsp','apimanager.adminFeature.ManageApis.description',0,'apimanager','APIM',NULL,NULL,4);
 
 --
 -- Data for table core_user_right
@@ -32,7 +34,7 @@ INSERT INTO core_user_right (id_right,id_user) VALUES ('APIMANAGER_API_MANAGEMEN
 --
 DELETE FROM core_admin_right WHERE id_right = 'APIMANAGER_PLAN_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('APIMANAGER_PLAN_MANAGEMENT','apimanager.adminFeature.ManagePlans.name',1,'jsp/admin/plugins/apimanager/ManagePlans.jsp','apimanager.adminFeature.ManagePlans.description',0,'apimanager',NULL,NULL,NULL,4);
+    ('APIMANAGER_PLAN_MANAGEMENT','apimanager.adminFeature.ManagePlans.name',1,'jsp/admin/plugins/apimanager/ManagePlans.jsp','apimanager.adminFeature.ManagePlans.description',0,'apimanager','APIM',NULL,NULL,4);
 
 --
 -- Data for table core_user_right
@@ -44,7 +46,7 @@ INSERT INTO core_user_right (id_right,id_user) VALUES ('APIMANAGER_PLAN_MANAGEME
 --
 DELETE FROM core_admin_right WHERE id_right = 'APIMANAGER_INSTANCE_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('APIMANAGER_INSTANCE_MANAGEMENT','apimanager.adminFeature.ManageInstances.name',1,'jsp/admin/plugins/apimanager/ManageInstances.jsp','apimanager.adminFeature.ManageInstances.description',0,'apimanager',NULL,NULL,NULL,4);
+    ('APIMANAGER_INSTANCE_MANAGEMENT','apimanager.adminFeature.ManageInstances.name',1,'jsp/admin/plugins/apimanager/ManageInstances.jsp','apimanager.adminFeature.ManageInstances.description',0,'apimanager','APIM',NULL,NULL,4);
 
 
 --
@@ -59,7 +61,7 @@ INSERT INTO core_user_right (id_right,id_user) VALUES ('APIMANAGER_INSTANCE_MANA
 --
 DELETE FROM core_admin_right WHERE id_right = 'APIMANAGER_SUBSCRIPTION_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('APIMANAGER_SUBSCRIPTION_MANAGEMENT','apimanager.adminFeature.ManageSubscriptions.name',1,'jsp/admin/plugins/apimanager/ManageSubscriptions.jsp','apimanager.adminFeature.ManageSubscriptions.description',0,'apimanager',NULL,NULL,NULL,4);
+    ('APIMANAGER_SUBSCRIPTION_MANAGEMENT','apimanager.adminFeature.ManageSubscriptions.name',1,'jsp/admin/plugins/apimanager/ManageSubscriptions.jsp','apimanager.adminFeature.ManageSubscriptions.description',0,'apimanager','APIM',NULL,NULL,4);
 
 
 --
@@ -74,7 +76,7 @@ INSERT INTO core_user_right (id_right,id_user) VALUES ('APIMANAGER_SUBSCRIPTION_
 --
 DELETE FROM core_admin_right WHERE id_right = 'APIMANAGER_OPERATION_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('APIMANAGER_OPERATION_MANAGEMENT','apimanager.adminFeature.ManageOperations.name',1,'jsp/admin/plugins/apimanager/ManageOperations.jsp','apimanager.adminFeature.ManageOperations.description',0,'apimanager',NULL,NULL,NULL,5);
+    ('APIMANAGER_OPERATION_MANAGEMENT','apimanager.adminFeature.ManageOperations.name',1,'jsp/admin/plugins/apimanager/ManageOperations.jsp','apimanager.adminFeature.ManageOperations.description',0,'apimanager','APIM',NULL,NULL,5);
 
 
 --
@@ -90,7 +92,7 @@ INSERT INTO core_user_right (id_right,id_user) VALUES ('APIMANAGER_OPERATION_MAN
 --
 DELETE FROM core_admin_right WHERE id_right = 'APIMANAGER_MEECROGATE_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('APIMANAGER_MEECROGATE_MANAGEMENT','apimanager.adminFeature.ManageMeecrogates.name',1,'jsp/admin/plugins/apimanager/ManageMeecrogates.jsp','apimanager.adminFeature.ManageMeecrogates.description',0,'apimanager',NULL,NULL,NULL,6);
+    ('APIMANAGER_MEECROGATE_MANAGEMENT','apimanager.adminFeature.ManageMeecrogates.name',1,'jsp/admin/plugins/apimanager/ManageMeecrogates.jsp','apimanager.adminFeature.ManageMeecrogates.description',0,'apimanager','APIM',NULL,NULL,6);
 
 
 --
