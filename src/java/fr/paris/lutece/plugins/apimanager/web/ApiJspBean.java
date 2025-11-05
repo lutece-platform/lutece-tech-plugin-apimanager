c127b04b3ce490f1e89886b51bf74aad04d2d862
oz <ococucci@yupiik.com>
2025-11-05 08:35:15 +0100

    #31116 : after  field a new resource  in the modification api screen  if you try to add an other resource  the form fields of the first resource are erased

--- src/java/fr/paris/lutece/plugins/apimanager/web/ApiJspBean.java
+++ src/java/fr/paris/lutece/plugins/apimanager/web/ApiJspBean.java
@@ -644,7 +644,6 @@ public class ApiJspBean extends AbstractJspBean<String, Api> {
         String usecase = request.getParameter(PARAMETER_CREATE_USECASE);
 
 
-        if (usecase != null && usecase.isEmpty()) {
         try {
             populateEnvironement(_api, request, getLocale());
         } catch (JsonProcessingException e) {
@@ -652,6 +651,7 @@ public class ApiJspBean extends AbstractJspBean<String, Api> {
             return redirect(request, VIEW_CREATE_API);
         }
 
+        if (usecase != null && usecase.isEmpty()) {
             _api = (_api != null) ? _api : new Api();
 
             if (!SecurityTokenService.getInstance().validate(request, ACTION_CREATE_API)) {
